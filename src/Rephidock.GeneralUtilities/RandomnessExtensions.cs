09f6e9d445cc48406f83f5c0cf0c0ca306e288af
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-02-02 22:03:29 +0100

    Added RandomnessExtensions.NextUInt31

---
+++ src/Rephidock.GeneralUtilities/RandomnessExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Rephidock.GeneralUtilities;
+
+
+/// <summary>
+/// Provides extensions for <see cref="Random"/>
+/// </summary>
+public static class RandomnessExtensions {
+
+	/// <summary>
+	/// Returns a non-negative random <see cref="int"/>.
+	/// Unlike <see cref="Random.Next()"/>, includes
+	/// <see cref="int.MaxValue"/> as a possible value.
+	/// </summary>
+	public static int NextUInt31(this Random rng) {
+
+		// Generate random bytes
+		Span<byte> seedBytes = stackalloc byte[4];
+		rng.NextBytes(seedBytes);
+
+		// Strip the int32 sign bit
+		seedBytes[3] &= 0b0111_1111;
+
+		// Convert and return
+		return BitConverter.ToInt32(seedBytes);
+	}
+
+}
