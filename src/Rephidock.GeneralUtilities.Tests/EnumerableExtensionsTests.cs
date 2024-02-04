a7b325515c6774528203ba9ef44ff976510bf58b
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-02-04 17:57:33 +0100

    Created tests for Yield

---
+++ src/Rephidock.GeneralUtilities.Tests/EnumerableExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Rephidock.GeneralUtilities;
+
+
+namespace Rephidock.GeneralUtilities.Tests;
+
+
+public sealed class EnumerableExtensionsTests {
+
+	#region //// Yield
+
+	[Fact]
+	public void Yield_UseOnReferenceTypes_ReturnSingleLengthEnumerableWithEqualRefernce() {
+
+		// Arrange
+		object someObject = new();
+
+		// Act
+		IEnumerable<object> enumerable = someObject.Yield();
+
+		// Assert
+		Assert.Single(enumerable);
+		Assert.Same(someObject, enumerable.First());
+
+	}
+
+	[Fact]
+	public void Yield_UseOnValueTypes_ReturnSingleLengthEnumerableWithEqualValue() {
+
+		// Arrange
+		int someValue = 64;
+
+		// Act
+		IEnumerable<int> enumerable = someValue.Yield();
+
+		// Assert
+		Assert.Single(enumerable);
+		Assert.Equal(someValue, enumerable.First());
+
+	}
+
+	#endregion
+
+}
