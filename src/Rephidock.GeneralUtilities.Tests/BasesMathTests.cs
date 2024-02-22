b0d3c36e929b016b395819af8d156a82a4c1f792
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-02-22 23:08:24 +0100

    BasesMath.CountAllAccending: Created

---
+++ src/Rephidock.GeneralUtilities.Tests/BasesMathTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+
+namespace Rephidock.GeneralUtilities.Tests;
+
+
+public sealed class BasesMathTests {
+
+	#region //// CountAllAccending
+
+	readonly Dictionary<string, ushort[][]> expectedCounterResults = new() {
+		{
+			"bin3places", new ushort[][] {
+				new ushort[] { 0, 0, 0 },
+				new ushort[] { 0, 0, 1 },
+				new ushort[] { 0, 1, 0 },
+				new ushort[] { 0, 1, 1 },
+				new ushort[] { 1, 0, 0 },
+				new ushort[] { 1, 0, 1 },
+				new ushort[] { 1, 1, 0 },
+				new ushort[] { 1, 1, 1 },
+			}
+		},
+		{
+			"quad2places",  new ushort[][] {
+				new ushort[] { 0, 0 },
+				new ushort[] { 0, 1 },
+				new ushort[] { 0, 2 },
+				new ushort[] { 0, 3 },
+				new ushort[] { 1, 0 },
+				new ushort[] { 1, 1 },
+				new ushort[] { 1, 2 },
+				new ushort[] { 1, 3 },
+				new ushort[] { 2, 0 },
+				new ushort[] { 2, 1 },
+				new ushort[] { 2, 2 },
+				new ushort[] { 2, 3 },
+				new ushort[] { 3, 0 },
+				new ushort[] { 3, 1 },
+				new ushort[] { 3, 2 },
+				new ushort[] { 3, 3 },
+			}
+		}
+	};
+
+	[Theory]
+	[InlineData(2, 3, "bin3places")]
+	[InlineData(4, 2, "quad2places")]
+	public void CountAllAccending_SimpleUse_CorrectReturn(ushort @base, int places, string expectedKey) {
+
+		// Arrange
+		IEnumerable<ushort[]> expected = expectedCounterResults[expectedKey];
+
+		// Act
+		IEnumerable<ushort[]> actual = BasesMath.CountAllAccending(@base, places);
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData(2)]
+	[InlineData(4)]
+	[InlineData(16)]
+	[InlineData(89)]
+	[InlineData(99)]
+	[InlineData(ushort.MaxValue)]
+	public void CountAllAccending_SinglePlace_ReturnsNumbesUpToNotIncludingBase(ushort @base) {
+
+		// Arrange
+		IEnumerable<ushort[]> expected = Enumerable
+											.Range(0, @base)
+											.Select(n => new ushort[] { (ushort)n });
+
+		// Act
+		IEnumerable<ushort[]> actual = BasesMath.CountAllAccending(@base, 1);
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
+
+	#endregion
+
+}
---
+++ src/Rephidock.GeneralUtilities/BasesMath.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Rephidock.GeneralUtilities;
+
+
+/// <summary>
+/// Provides methods to operate on numbers
+/// in arbitrary bases
+/// </summary>
+public static class BasesMath {
+
+	/// <summary>
+	/// <para>
+	/// Enumerates all positive numbers in an arbitaray base
+	/// up to a given digit length,
+	/// returning arrays of digits in that base,
+	/// units place last.
+	/// </para>
+	/// <para>
+	/// Starts with array of zeros.
+	/// Digit at the end of the array is incremented first.
+	/// </para>
+	/// </summary>
+	/// <param name="base">The base of the counter (exclusive maxim value of individual digits)</param>
+	/// <param name="places">The number of the digits in the counter</param>
+	/// <exception cref="ArgumentException">
+	/// <paramref name="base"/> is smaller than 2 or
+	/// <paramref name="places"/> is smaller than 1
+	/// </exception>
+	public static IEnumerable<ushort[]> CountAllAccending(ushort @base, int places) {
+
+		// Guards
+		if (places < 1) {
+			throw new ArgumentException("There must be at least one place in the counter", nameof(places));
+		}
+
+		if (@base < 2) {
+			throw new ArgumentException("Base must be at least 2", nameof(@base));
+		}
+
+		// Create current with all 0 
+		ushort[] currentCount = new ushort[places];
+
+		// Main loop
+		while (true) {
+
+			// Return a copy
+			yield return currentCount.ToArray();
+
+			// Add one
+			bool carry = true;
+			for (int i = places - 1; i >= 0; i--) {
+
+				// Carry the carry to the next place
+				if (currentCount[i] == @base - 1) {
+					currentCount[i] = 0;
+					continue;
+				}
+
+				// Consume the carry
+				currentCount[i] += 1;
+				carry = false;
+				break;
+
+			}
+
+			// Overflowed to 0 -- counting ended
+			if (carry) break;
+
+		}
+
+	}
+
+}
+
