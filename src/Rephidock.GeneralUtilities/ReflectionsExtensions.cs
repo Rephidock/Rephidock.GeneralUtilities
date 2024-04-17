cdcefcf76e9a386fbd4c01e2d51f0b484cf54d8d
Rephi <83082012+Rephidock@users.noreply.github.com>
2024-04-17 21:36:55 +0200

    Downgraded ReflectionExtensions

--- src/Rephidock.GeneralUtilities/ReflectionsExtensions.cs
+++ src/Rephidock.GeneralUtilities/ReflectionsExtensions.cs
@@ -26,7 +26,7 @@ public static class ReflectionExtensions {
 	/// </remarks>
 	/// <exception cref="ArgumentNullException"><paramref name="baseType"/> is null</exception>
 	/// <exception cref="NotSupportedException"><paramref name="baseType"/> is an interface or (netframework35) <paramref name="derivedType"/> is an enum</exception>
-	public static bool IsSubclassOrSelfOf(this Type? derivedType, Type baseType) {
+	public static bool IsSubclassOrSelfOf(this Type derivedType, Type baseType) {
 
 		// Guards
 		if (baseType == null) {
@@ -63,10 +63,10 @@ public static class ReflectionExtensions {
 
 	}
 
-	/// <inheritdoc cref="IsSubclassOrSelfOf(Type?, Type)"/>
+	/// <inheritdoc cref="IsSubclassOrSelfOf(Type, Type)"/>
 	[EditorBrowsable(EditorBrowsableState.Never)]
 	[Obsolete("Method is obsolete due to a spelling error. Use IsSubclassOrSelfOf instead.")]
-	public static bool IsSubcalssOrSelfOf(this Type? derivedType, Type baseType) {
+	public static bool IsSubcalssOrSelfOf(this Type derivedType, Type baseType) {
 		return IsSubclassOrSelfOf(derivedType, baseType);
 	}
 
